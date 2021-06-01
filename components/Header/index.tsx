9182ed993b4e11324216b0f56014cc193e95c125
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-06-01 09:39:00 +0100

    Built header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="container mb-8 py-6">
+      <div className="font-cursive flex mr-auto items-center">
+        <span className="mr-1 text-2xl">DogWiki</span>
+        <img className="w-10" src="/logo.png" alt="Logo" />
+      </div>
+    </header>
+  );
+}
